3a6a3ee729b07025bab693d608908bfc69a0a173
Yana-Koroliuk <yana.koroliuk1@gmail.com>
2023-12-12 19:15:34 +0200

    Update book and language services

--- src/main/java/ua/training/service/BookService.java
+++ src/main/java/ua/training/service/BookService.java
@@ -7,12 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ua.training.model.Book;
-import ua.training.model.BookTranslate;
-import ua.training.model.BookWithTranslate;
-import ua.training.model.Language;
+import ua.training.model.*;
 import ua.training.repository.BookRepository;
 import ua.training.repository.BookTranslateRepository;
+import ua.training.repository.UserRepository;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,11 +22,13 @@ public class BookService {
 
     private final BookRepository bookRepository;
     private final BookTranslateRepository bookTranslateRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public BookService(BookRepository bookRepository, BookTranslateRepository bookTranslateRepository) {
+    public BookService(BookRepository bookRepository, BookTranslateRepository bookTranslateRepository, UserRepository userRepository) {
         this.bookRepository = bookRepository;
         this.bookTranslateRepository = bookTranslateRepository;
+        this.userRepository = userRepository;
     }
 
     public void addBook(Book book) {
@@ -37,6 +37,30 @@ public class BookService {
 
     public List<BookWithTranslate> findPaginatedAndLocated(int pageNo, int pageSize, Language language) {
         Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by("id"));
+        Page<Book> pagedResult = bookRepository.findAll(paging);
+        List<Book> books = pagedResult.toList();
+        List<BookWithTranslate> bookWithTranslateList = new ArrayList<>();
+        for (Book book : books) {
+            BookTranslate bookTranslate = bookTranslateRepository.findByBookAndLanguage(book, language).orElseThrow(() -> new RuntimeException("There is no such translate"));
+            BookWithTranslate bookWithTranslate = new BookWithTranslate(book, bookTranslate);
+            bookWithTranslateList.add(bookWithTranslate);
+        }
+        return bookWithTranslateList;
+    }
+
+    public List<BookWithTranslate> findPaginatedAndLocatedWithSortByAndSortType(int pageNo, int pageSize, Language language, String sortBy, String sortType) {
+        if (sortBy == null || sortBy.trim().equals("")) {
+            sortBy = "id";
+        }
+        if (sortType == null || sortType.trim().equals("")) {
+            sortType = "asc";
+        }
+        Pageable paging;
+        if (sortType.trim().equals("dec")) {
+            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+        } else {
+            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        }
         Page<Book> pagedResult = bookRepository.findAll(paging);
         List<Book> books = pagedResult.toList();
         List<BookWithTranslate> bookWithTranslateList = new ArrayList<>();
@@ -67,5 +91,46 @@ public class BookService {
 
     public void updateBook(Book book) {
         bookRepository.save(book);
+    }
+
+    public BookWithTranslate findByIdLocated(long id, Language language) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no such book"));
+        BookTranslate bookTranslate = bookTranslateRepository.findByBookAndLanguage(book, language)
+                .orElseThrow(() -> new RuntimeException("There is no such book translate"));
+        return new BookWithTranslate(book, bookTranslate);
+    }
+
+    public int getAmountOfBooksByKeyWords(String keyWords, Language language) {
+        AtomicInteger amount = new AtomicInteger();
+        bookTranslateRepository
+                .findAllByTitleContainsOrAuthorsStringContainsIgnoreCaseAndLanguage(keyWords, keyWords, language).forEach((p) -> amount.getAndIncrement());
+        return Integer.parseInt(amount.toString());
+    }
+
+    public List<BookWithTranslate> findPaginatedAndLocatedByKeyWords(String keyWords, Language language, int pageNo,
+                                                                     int pageSize, String sortBy, String sortType) {
+        if (sortBy == null || sortBy.trim().equals("")) {
+            sortBy = "id";
+        }
+        if (sortType == null || sortType.trim().equals("")) {
+            sortType = "asc";
+        }
+        Pageable paging;
+        if (sortType.trim().equals("dec")) {
+            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).descending());
+        } else {
+            paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        }
+        Page<BookTranslate> page = bookTranslateRepository.findAllByTitleContainsOrAuthorsStringContainsIgnoreCaseAndLanguage(keyWords,
+                keyWords, language, paging);
+        List<BookTranslate> books = page.toList();
+        List<BookWithTranslate> bookWithTranslateList = new ArrayList<>();
+        for (BookTranslate bookTranslate : books) {
+            long bookId = bookTranslate.getBook().getId();
+            Book book = bookRepository.findById(bookId).orElseThrow(() -> new RuntimeException("There is no such book"));
+            BookWithTranslate bookWithTranslate = new BookWithTranslate(book, bookTranslate);
+            bookWithTranslateList.add(bookWithTranslate);
+        }
+        return bookWithTranslateList;
     }
 }
--- src/main/java/ua/training/service/LanguageService.java
+++ src/main/java/ua/training/service/LanguageService.java
@@ -1,11 +1,13 @@
 package ua.training.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import ua.training.model.Language;
 import ua.training.repository.LanguageRepository;
 
 import javax.annotation.PostConstruct;
+import java.util.Locale;
 import java.util.Optional;
 
 @Service
@@ -21,6 +23,14 @@ public class LanguageService {
     public Optional<Language> findByName(String name) {
         return languageRepository.findByName(name);
     }
+
+    public Language getCurrentLanguage() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return findByName(locale.getLanguage())
+                .orElseThrow(() -> new RuntimeException("There is no such language"));
+    }
+
+//    public Locale
 //    @PostConstruct
 //    private void initLanguages() {
 //        Language uk = new Language("uk");
